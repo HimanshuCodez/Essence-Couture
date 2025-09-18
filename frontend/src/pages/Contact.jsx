0fc627607384459ca18a460e2a43dcb1be4528d7
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-18 22:36:31 +0530

    changes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@Essence Couture.com</p>
+          <p className=' text-gray-500'>C5a/205 <br />Janakpuri Delhi 110042</p>
+          <p className=' text-gray-500'>Tel: 9958928568<br /> Email: rita@essencecouture.co.in</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Essence Couture</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
