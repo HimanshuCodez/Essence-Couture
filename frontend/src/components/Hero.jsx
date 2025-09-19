1737a1604940dccad0cd61c067f2f0d980ca02f9
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-19 22:11:46 +0530

    about us

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 
 const Hero = () => {
   const slides = [
-    "https://wforwoman.com/cdn/shop/files/The_Onam_Edit-banner.jpg?v=1755604463&width=1500",
+    "https://imgs.search.brave.com/JZLMs1CuNq_UnJiXSoTcJfYXisr13k6VtCcHANJ372o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/c2hvcGlmeS5jb20v/cy9maWxlcy8xLzA1/ODYvOTIwNy81NzAz/L2ZpbGVzL25hdnJh/dHJpLXNwZWNpYWwu/anBnP3Y9MTc0MzE1/ODA3MQ",
     "https://wforwoman.com/cdn/shop/files/end_of_season_sale_banner.jpg?v=1749802725&width=1500",
     "https://wishfulbyw.com/cdn/shop/files/banner_copy_4.jpg?v=1749821233&width=1500",
     "https://wishfulbyw.com/cdn/shop/files/banner_6.jpg?v=1730876675&width=1500",
@@ -33,7 +33,7 @@ const Hero = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="w-screen h-[40vh] sm:h-[60vh] lg:h-screen overflow-hidden relative">
+      <div className="w-screen h-[50vh] sm:h-[60vh] lg:h-[70vh] overflow-hidden relative">
         <div
           className="flex transition-transform duration-500 ease-in-out h-full"
           ref={sliderRef}
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,49 +1,84 @@
-import React from 'react'
-import Title from '../components/Title'
-import { assets } from '../assets/assets'
-import NewsletterBox from '../components/NewsletterBox'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const About = () => {
   return (
     <div>
-
-      <div className='text-2xl text-center pt-8 border-t'>
-          <Title text1={'ABOUT'} text2={'US'} />
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
       </div>
 
-      <div className='my-10 flex flex-col md:flex-row gap-16'>
-          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
-          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
-              <p>Essence Couture was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
-              <p>Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.</p>
-              <b className='text-gray-800'>Our Mission</b>
-              <p>Our mission at Essence Couture is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
-          </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Essence Couture by Rita is a distinguished new brand under the
+            esteemed umbrella of Rita and Company, dedicated to celebrating the
+            rich traditions of Indian ethnic wear while embracing contemporary
+            elegance. With an impeccable fusion of timeless craftsmanship and
+            modern design sensibilities, Essence Couture offers an exquisite
+            collection of clothing for women and kids .
+          </p>
+          <p>
+            Our creations embody the essence of luxury, sophistication, and
+            cultural heritage, meticulously designed to cater to the discerning
+            tastes of today’s fashion-forward individuals. Whether it’s the
+            intricacy of traditional embroidery, the vibrancy of Indian
+            textiles, or the fusion of global styles, each piece from Essence
+            Couture tells a story of artistry, grace, and refinement.
+          </p>
+
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            At Essence Couture, we believe in crafting not just garments but
+            experiences, celebrating every occasion with the perfect blend of
+            opulence and comfort. Each collection is carefully curated to cater
+            to the diverse needs of our customers, ensuring that every piece is
+            as unique as the individual who wears it.
+          </p>
+          <p>
+            Explore the world of Essence Couture and indulge in the luxury of truly exceptional fashion, where tradition meets modernity.
+          </p>
+        </div>
       </div>
 
-      <div className=' text-xl py-4'>
-          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      <div className=" text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
       </div>
 
-      <div className='flex flex-col md:flex-row text-sm mb-20'>
-          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-            <b>Quality Assurance:</b>
-            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
-          </div>
-          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-            <b>Convenience:</b>
-            <p className=' text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
-          </div>
-          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-            <b>Exceptional Customer Service:</b>
-            <p className=' text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
-          </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className=" text-gray-600">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className=" text-gray-600">
+            With our user-friendly interface and hassle-free ordering process,
+            shopping has never been easier.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className=" text-gray-600">
+            Our team of dedicated professionals is here to assist you the way,
+            ensuring your satisfaction is our top priority.
+          </p>
+        </div>
       </div>
 
-      <NewsletterBox/>
-      
+      <NewsletterBox />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
