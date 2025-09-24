b358244b702cb43fa738325c11c31a1cf5773a13
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-24 13:37:13 +0530

    changes

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -60,7 +60,9 @@ const Product = () => {
           </div>
           <div className='flex items-baseline gap-3 mt-5'>
             <p className='text-3xl font-medium'>{currency}{productData.price}</p>
-            <p className='text-xl text-gray-500 line-through'>{currency}{productData.mrp}</p>
+            {productData.mrp && productData.mrp !== productData.price && (
+              <p className='text-xl text-gray-500 line-through'>{currency}{productData.mrp}</p>
+            )}
           </div>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
           <div className='flex flex-col gap-4 my-8'>
@@ -110,8 +112,8 @@ const Product = () => {
           <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
         </div>
         <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
-          <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
-          <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
+         <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+     
         </div>
       </div>
 
